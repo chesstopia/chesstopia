e6216ed7730bb6bbacc57d75182b3330eab66126
Marcus Kraft <marcus_kraft@posteo.de>
2026-06-15 20:49:59 +0200

    CHESS-6: auto configure embedded database for test

--- chesstopia-backend/src/test/java/io/chesstopia/backend/ChessTopiaApplicationTests.java
+++ chesstopia-backend/src/test/java/io/chesstopia/backend/ChessTopiaApplicationTests.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureEmbeddedDatabase
 class ChessTopiaApplicationTests {
 
     @Test
